8f9941b43ac6ef92efa3ee2eff8a669f22c9f4fb
egoisticfemboy <egoisticdoor@gmail.com>
2024-07-10 08:53:52 +0200

    Update login_config.py

--- src/login_config.py
+++ src/login_config.py
@@ -1,19 +1,6 @@
 import subprocess
 import winreg
 
-def enable_virtual_keyboard():
-    try:
-        reg_key = r"Software\Microsoft\Windows\CurrentVersion\Run"
-        
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, reg_key, 0, winreg.KEY_SET_VALUE)
-        
-        winreg.SetValueEx(key, "Osk", 0, winreg.REG_SZ, "C:\\Windows\System32\osk.exe")
-        winreg.CloseKey(key)
-        
-        print("Virtual keyboard enabled")
-        
-    except Exception as e:
-        print(f"Failed to enable virtual keyboard: {e}")
         
 def disable_power_button():
     try:
